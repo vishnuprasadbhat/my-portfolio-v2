73fe7fbb3a875bd053c91c5601bda88e8a52e643
vishnuprasadbhat <bhatvishnu.1@gmail.com>
2024-07-25 14:08:54 +0530

    fix: work link

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,13 +1,9 @@
 import React from "react";
+import Button from "../Button";
 
 const WorkCard = ({ img, name, description, url, onClick }) => {
   return (
-    <div
-      className={`overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link ${
-        url ? "cursor-pointer" : "cursor-not-allowed"
-      }`}
-      onClick={onClick}
-    >
+    <div className={`overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0`}>
       <div
         className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
         style={{ height: "300px" }}
@@ -18,9 +14,15 @@ const WorkCard = ({ img, name, description, url, onClick }) => {
           src={img}
         ></img>
       </div>
-      <h1 className="mt-5 text-3xl font-medium">
+      <Button
+        type="link"
+        onClick={onClick}
+        classes={`btn-underline pb-0.5 mt-5 px-0 ml-0 laptop:ml-0 mob:text-2xl laptop:text-2xl font-medium ${
+          url ? "cursor-pointer" : "cursor-not-allowed"
+        }`}
+      >
         {name ? name : "Project Name"}
-      </h1>
+      </Button>
       <h2 className="mob:text-justify text-xl opacity-50">
         {description ? description : "Description"}
       </h2>
