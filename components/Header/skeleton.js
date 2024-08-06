dfc02a1ab5ba0e4d5f127456431a6e92d202a11e
vishnuprasadbhat <bhatvishnu.1@gmail.com>
2024-08-06 14:52:14 +0530

    fix: skeleton shimmer effect

--- components/Header/skeleton.js
+++ components/Header/skeleton.js
@@ -1,24 +1,9 @@
 import Skeleton from "react-loading-skeleton";
-const shimmer =
-  "rounded skeleton skeleton-bg dark:dark-skeleton-bg bg-gradient-skeleton dark:bg-gradient-dark-skeleton";
+const shimmer = "rounded skeleton";
 export default function HeaderSkeleton() {
   return (
-    <div
-      className={`mt-10 pt-2 flex flex-row items-center justify-between sticky top-0 z-10`}
-    >
-      <div>
-        <Skeleton className={`w-16 h-8 ${shimmer}`} />
-      </div>
-      <div className=" flex items-center justify-between gap-4">
-        <Skeleton className={`h-8 w-16 hidden tablet:span ${shimmer}`} />
-        <Skeleton className={`h-8 w-16 hidden tablet:span ${shimmer}`} />
-        <Skeleton className={`h-8 w-16 hidden tablet:span ${shimmer}`} />
-        <Skeleton className={`h-8 w-16 hidden tablet:span ${shimmer}`} />
-        <Skeleton className={`h-8 w-16 hidden tablet:span ${shimmer}`} />
-        <Skeleton className={`h-8 w-8 ${shimmer}`} />
-        <Skeleton className={`h-8 w-8 ${shimmer}`} />
-        <Skeleton className={`h-8 w-8 tablet:hidden ${shimmer}`} />
-      </div>
+    <div className={`h-12 mt-10 sticky top-0 z-10`}>
+      <Skeleton className={`w-full h-12 ${shimmer}`} />
     </div>
   );
 }
